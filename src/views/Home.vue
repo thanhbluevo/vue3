91e80fd2dda30bc077fd34c39c492f72e0fd2a0a
ThanhBlue <thanh.vo@whl.travel>
2020-11-22 16:18:35 +0700

    RRRR

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
+<div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" :details="details" :variants="variants" />
+</div>
 </template>
 
 <script>
@@ -10,9 +10,33 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    name: "Home",
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            details: [
+                '50% cotton',
+                '30% wool',
+                '20% polyester',
+            ],
+            variants: [{
+                    id: 2212,
+                    color: 'green',
+                    image: '../assets/images/socks_green.jpg',
+                    quanlity: 50,
+                    active: false
+                },
+                {
+                    id: 2213,
+                    color: 'blue',
+                    image: '../assets/images/socks_blue.jpg',
+                    quanlity: 0,
+                    active: true
+                },
+            ],
+        }
+    },
 };
 </script>
